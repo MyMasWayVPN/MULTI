22d906b703c2c3f9e366d5c16237db08e542c486
Khoirul Bahra <107555389+MyMasWayVPN@users.noreply.github.com>
2023-03-05 06:56:21 +0700

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -5,8 +5,8 @@ clear
 echo start
 sleep 0.5
 source /var/lib/ipvps.conf
-domain=$(cat /usr/local/etc/xray/domain)
-emailcf=$(cat /usr/local/etc/xray/email)
+domain=$(cat /etc/xray/domain)
+emailcf=$(cat /etc/xray/email)
 clear
 systemctl stop xray
 echo -e "\e[0;32mStart renew your Certificate SSL\e[0m"
@@ -14,7 +14,7 @@ sleep 1
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /usr/local/etc/xray/xray.crt --keypath /usr/local/etc/xray/xray.key --ecc
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
 systemctl start xray
 echo Done
 sleep 0.5
