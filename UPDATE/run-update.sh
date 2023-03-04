0066fa7918eac947e3df59368d32708c4747d3b0
Khoirul Bahra <107555389+MyMasWayVPN@users.noreply.github.com>
2023-03-05 05:51:37 +0700

    Add files via upload

---
+++ UPDATE/run-update.sh
@@ -0,0 +1,237 @@
+#wget https://github.com/${GitUser}/
+GitUser="MyMasWayVPN"
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+colornow=$(cat /etc/tarap/theme/color.conf)
+NC="\e[0m"
+RED="\033[0;31m"
+COLOR1="$(cat /etc/tarap/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/tarap/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+WH='\033[1;37m'
+###########- END COLOR CODE -##########
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/MyMasWayVPN/MyMasWayVPN.github.io/main/wkwkwkwk > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/MyMasWayVPN/MyMasWayVPN.github.io/main/wkwkwkwk | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/MyMasWayVPN/MyMasWayVPN.github.io/main/wkwkwkwk | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
+clear
+echo ""
+version=$(cat /home/ver)
+ver=$( curl https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/version )
+clear
+# LINE COLOUR
+line=$(cat /etc/line)
+# TEXT COLOUR BELOW
+below=$(cat /etc/below)
+# BACKGROUND TEXT COLOUR
+back_text=$(cat /etc/back)
+# NUMBER COLOUR
+number=$(cat /etc/number)
+# TEXT ON BOX COLOUR
+box=$(cat /etc/box)
+# CEK UPDATE
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info1="${Green_font_prefix}($version)${Font_color_suffix}"
+Info2="${Green_font_prefix}(LATEST VERSION)${Font_color_suffix}"
+Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
+version=$(cat /home/ver)
+new_version=$( curl https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/version | grep $version )
+#Status Version
+if [ $version = $new_version ]; then
+sts="${Info2}"
+else
+sts="${Error}"
+fi
+clear
+echo ""
+echo -e "   \e[$line--------------------------------------------------------\e[m"
+echo -e "   \e[$back_text                 \e[30m[\e[$box CHECK NEW UPDATE\e[30m ]                   \e[m"
+echo -e "   \e[$line--------------------------------------------------------\e[m"
+echo -e "   \e[$below VERSION NOW >> $Info1"
+echo -e "   \e[$below STATUS UPDATE >> $sts"
+echo -e ""
+echo -e "       \e[1;31mWould you like to proceed?\e[0m"
+echo ""
+echo -e "            \e[0;32m[ Select Option ]\033[0m"
+echo -e "     \e[$number [1]\e[m \e[$below Check Script Update Now\e[m"
+echo -e "     \e[$number [x]\e[m \e[$below Back To Update Menu\e[m"
+echo -e "     \e[$number [y]\e[m \e[$below Back To Main Menu\e[m"
+echo -e ""
+echo -e "   \e[$line--------------------------------------------------------\e[m"
+echo -e "\e[$line"
+read -p "Please Choose 1 or x & y : " option2
+case $option2 in
+1)
+version=$(cat /home/ver)
+new_version=$( curl https://raw.githubusercontent.com/${GitUser}/MULTI/main/version | grep $version )
+if [ $version = $new_version ]; then
+clear
+echo ""
+echo -e "\e[1;31mChecking New Version, Please Wait...!\e[m"
+sleep 3
+clear
+echo -e "\e[1;31mUpdate Not Available\e[m"
+echo ""
+clear
+sleep 1
+echo -e "\e[1;36mYou Have The Latest Version\e[m"
+echo -e "\e[1;31mThankyou.\e[0m"
+sleep 2
+update
+fi
+clear
+echo -e "\e[1;31mUpdate Available Now..\e[m"
+echo -e ""
+sleep 2
+echo -e "\e[1;36mStart Update For New Version, Please Wait..\e[m"
+sleep 2
+clear
+echo -e "\e[0;32mGetting New Version Script..\e[0m"
+sleep 1
+echo ""
+# UPDATE RUN-UPDATE
+cd /usr/bin
+wget -O run-update "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/UPDATE/run-update.sh"
+chmod +x run-update
+# RUN UPDATE
+echo ""
+clear
+echo -e "\e[0;32mPlease Wait...!\e[0m"
+sleep 6
+clear
+echo ""
+echo -e "\e[0;32mNew Version Downloading started!\e[0m"
+sleep 2
+cd /usr/bin
+wget -O update "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/UPDATE/update.sh"
+wget -O run-update "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/UPDATE/run-update.sh"
+wget -O message-ssh "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/UPDATE/message-ssh.sh"
+wget -O change-port "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/change.sh"
+wget -O system "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/menu/system.sh"
+wget -O menu "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/menu.sh"
+wget -O add-host "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/system/add-host.sh"
+wget -O check-sc "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/system/running.sh"
+wget -O cert "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/cert.sh"
+wget -O trojaan "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/MENU/trojaan.sh"
+wget -O xraay "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/MENU/xraay.sh"
+wget -O xp "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/xp.sh"
+wget -O port-xray "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/CHANGE-PORT/port-xray.sh"
+wget -O themes "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/MENU/themes.sh"
+wget -O autobackup "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/SYSTEM/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/SYSTEM/backup.sh"
+wget -O bckp "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/SYSTEM/bckp.sh"
+wget -O restore "https://raw.githubusercontent.com/MyMasWayVPN/MULTI/main/SYSTEM/restore.sh"
+chmod +x update
+chmod +x run-update
+chmod +x message-ssh
+chmod +x change-port
+chmod +x system
+chmod +x menu
+chmod +x add-host
+chmod +x check-sc
+chmod +x cert
+chmod +x trojaan
+chmod +x xraay
+chmod +x xp
+chmod +x port-xray
+chmod +x themes
+chmod +x autobackup
+chmod +x backup
+chmod +x bckp
+chmod +x restore
+clear
+echo -e ""
+echo -e "\e[0;32mDownloaded successfully!\e[0m"
+echo ""
+ver=$( curl https://raw.githubusercontent.com/${GitUser}/MULTI/main/version )
+sleep 1
+echo -e "\e[0;32mPatching New Update, Please Wait...\e[0m"
+echo ""
+sleep 2
+echo -e "\e[0;32mPatching... OK!\e[0m"
+sleep 1
+echo ""
+echo -e "\e[0;32mSucces Update Script For New Version\e[0m"
+cd
+echo "$ver" > /home/ver
+rm -f update.sh
+clear
+echo ""
+echo -e "\033[0;34m----------------------------------------\033[0m"
+echo -e "\E[44;1;39m            SCRIPT UPDATED              \E[0m"
+echo -e "\033[0;34m----------------------------------------\033[0m"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+;;
+x)
+clear
+update
+;;
+y)
+clear
+menu
+;;
+*)
+clear
+echo -e "\e[1;31mPlease Enter Option 1-2 or x & y Only..,Try again, Thank You..\e[0m"
+sleep 2
+run-update
+;;
+esac
